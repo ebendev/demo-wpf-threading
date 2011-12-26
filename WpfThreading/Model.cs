28cf0dc583db67c1c549a3b7df14e485bde1df62
Eben Geer <me@eben.co>
2011-12-26 01:55:07 -0600

    initial from disk

---
+++ WpfThreading/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace WpfThreading
+{
+    class Model
+    {
+        private Thread worker;
+        private Random random = new Random((int) DateTime.UtcNow.Ticks);
+
+        internal event Action<string> NewLine = delegate { }; 
+
+        public Model()
+        {
+            worker = new Thread(Work) { IsBackground = true };
+            worker.Start();
+        }
+
+        private void Work()
+        {
+            while(true)
+            {
+                NewLine(random.Next().ToString());
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ WpfThreading/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace WpfThreading
+{
+    class ViewModel
+    {
+        private Dispatcher dispatcher;
+
+        public ObservableCollection<string> Lines { get; set; }
+
+        public ViewModel(Model model)
+        {
+            dispatcher = Dispatcher.CurrentDispatcher;
+            Lines = new ObservableCollection<string>();
+            model.NewLine += model_NewLine;
+        }
+
+        private void model_NewLine(string obj)
+        {
+            dispatcher.BeginInvoke(new ThreadStart(() => Lines.Add(obj)));
+        }
+    }
+}
